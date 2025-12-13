2cf53f40b389b06b8fb0231efc84146ba21233f6
Akshay <mandaakshay20@gmail.com>
2025-12-13 15:38:33 +0530

    Wrote code according to the gieven description

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,78 @@
-
 import React from "react";
-import './../styles/App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import "../styles/App.css";
 
-const App = () => {
+const Home = () => {
+  return <div>Index</div>;
+};
+
+const Women = () => {
+  const { path, url } = useRouteMatch();
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Women Items:</p>
+      <ul>
+        <li>
+          <Link to={`${url}/grooming`}>Grooming</Link>
+        </li>
+        <li>
+          <Link to={`${url}/shirt`}>Shirt</Link>
+        </li>
+        <li>
+          <Link to={`${url}/trouser`}>Trouser</Link>
+        </li>
+        <li>
+          <Link to={`${url}/jewellery`}>Jewellery</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route path={`${path}/:item`}>
+          <Item />
+        </Route>
+      </Switch>
     </div>
-  )
+  );
+};
+
+const Item = () => {
+  const { item } = useParams();
+  return <div>{item}</div>;
+};
+
+function App() {
+  return (
+    <Router>
+      <div className="main-container">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/women">Women</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/women">
+            <Women />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
